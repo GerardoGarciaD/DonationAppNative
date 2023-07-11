9a4607e586188580fd68e5d59245cf25e6173f8f
Gerardo <geraz26@hotmail.com>
2023-07-11 10:52:06 -0600

    Creando reducer de donatios

--- redux/store.js
+++ redux/store.js
@@ -8,10 +8,12 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import User from './reducers/User';
 import Categories from './reducers/Categories';
+import Donations from './reducers/Donations';
 
 const rootReducer = combineReducers({
   user: User,
   categories: Categories,
+  donations: Donations,
 });
 
 const configuration = {

1389f72950c7fdbdf7a13b1945b1af2abd33774c
Gerardo <geraz26@hotmail.com>
2023-07-10 23:35:59 -0600

    Creando pagina principal

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyles from '../../assets/styles/globalStyle';
-import Header from '../../components/Header/Header';
 import {useDispatch, useSelector} from 'react-redux';
-import {Pressable, Text} from 'react-native';
-import {updateFirstName} from '../../redux/reducers/User';
-
+import {Image, Pressable, ScrollView, Text, View} from 'react-native';
+import style from './style';
+import Header from '../../components/Header/Header';
+import Search from '../../components/Search/Search';
 const Home = () => {
   const user = useSelector(state => state.user);
   const dispatch = useDispatch();
-  console.log(user);
   return (
     <SafeAreaView style={[globalStyles.backgroundWhite, globalStyles.flex]}>
-      <Header title={`${user.firstName} ${user.lastName}`} />
-
-      <Pressable
-        onPress={() => dispatch(updateFirstName({firstName: 'Pedro'}))}>
-        <Text>Cambiame el nombre</Text>
-      </Pressable>
+      <ScrollView>
+        <View style={style.header}>
+          <View>
+            <Text style={style.headerIntroText}>Hello, </Text>
+            <View style={style.userName}>
+              <Header title={`${user.firstName} ${user.lastName}. 👋`} />
+            </View>
+          </View>
+          <Image
+            source={{uri: user.profileImage}}
+            style={style.profileImage}
+            resizeMode="contain"></Image>
+        </View>
+        <View style={style.searchBox}>
+          <Search onSearch={() => {}} />
+        </View>
+        <Pressable style={style.highlightedImageContainer}>
+          <Image
+            style={style.highlightedImage}
+            source={require('../../assets/images/highlighted_image.png')}
+            resizeMode="contain"
+          />
+        </Pressable>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- screens/Home/style.js
+++ screens/Home/style.js
@@ -1,5 +1,45 @@
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/styles/scaling';
+
 const {StyleSheet} = require('react-native');
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    marginTop: verticalScale(20),
+    marginHorizontal: verticalScale(24),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  headerIntroText: {
+    fontFamily: 'Inter',
+    fontSize: scaleFontSize(16),
+    lineHeight: scaleFontSize(19),
+    fontWeight: '400',
+    color: '#636776',
+  },
+  userName: {
+    marginTop: verticalScale(5),
+  },
+  profileImage: {
+    width: horizontalScale(50),
+    height: verticalScale(50),
+  },
+  searchBox: {
+    marginTop: verticalScale(20),
+    marginHorizontal: verticalScale(24),
+  },
+  highlightedImageContainer: {
+    marginHorizontal: horizontalScale(24),
+  },
+  highlightedImage: {
+    width: '100%',
+    height: verticalScale(160),
+  },
+});
 
 export default styles;

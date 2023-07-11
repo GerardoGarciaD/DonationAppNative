c00590e536e1c3fa17f842d85764c01b33d14166
Gerardo <geraz26@hotmail.com>
2023-07-11 12:40:15 -0600

    Implementando boton y los estilos

--- screens/SingleDonationItem/SingleDonationItem.tsx
+++ screens/SingleDonationItem/SingleDonationItem.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import style from './style';
-import {Text} from 'react-native';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
 import {useSelector} from 'react-redux';
+import globalStyles from '../../assets/styles/globalStyle';
+import BackButton from '../../components/BackButton/BackButton';
+import {useNavigation} from '@react-navigation/native';
 
 const SingleDonationItem = () => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationId,
   );
+  const navigation = useNavigation();
 
-  console.log('donationItemInformation', donationItemInformation);
-
-  return <Text>SingleDonationItem</Text>;
+  return (
+    <SafeAreaView style={[globalStyles.backgroundWhite, globalStyles.flex]}>
+      <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SingleDonationItem;

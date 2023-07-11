6a993139227b69c250b703f6eefe474b4fe06a0b
Gerardo <geraz26@hotmail.com>
2023-07-10 22:41:21 -0600

    Creando store y reducers iniciales

---
+++ redux/store.js
@@ -0,0 +1,15 @@
+import {combineReducers} from 'redux';
+
+import {configureStore} from '@reduxjs/toolkit';
+
+import User from './reducers/User';
+
+const rootReducer = combineReducers({
+  user: User,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;

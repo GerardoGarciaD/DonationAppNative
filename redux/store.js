5e00e6dc87ff5f2cddeb9cb75f5eefabe66a12dd
Gerardo <geraz26@hotmail.com>
2023-07-10 23:04:21 -0600

    Configuración para hacer persitente la store

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,9 @@
 import {combineReducers} from 'redux';
 
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {configureStore} from '@reduxjs/toolkit';
 
 import User from './reducers/User';
@@ -8,8 +12,22 @@ const rootReducer = combineReducers({
   user: User,
 });
 
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
+
+export const persistor = persistStore(store);

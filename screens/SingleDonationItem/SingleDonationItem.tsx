5c84848c509a484af73387528af7453f385d4e47
Gerardo <geraz26@hotmail.com>
2023-07-11 13:12:31 -0600

    Creando utilizar los params que se envian en el enrutamiento y mostrando la información del state

--- screens/SingleDonationItem/SingleDonationItem.tsx
+++ screens/SingleDonationItem/SingleDonationItem.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import style from './style';
-import {SafeAreaView, ScrollView, Text} from 'react-native';
+import {Image, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import {useSelector} from 'react-redux';
 import globalStyles from '../../assets/styles/globalStyle';
 import BackButton from '../../components/BackButton/BackButton';
 import {useNavigation} from '@react-navigation/native';
+import Badge from '../../components/Badge/Badge';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
-const SingleDonationItem = () => {
-  const donationItemInformation = useSelector(
-    state => state.donations.selectedDonationId,
+const SingleDonationItem = ({route}) => {
+  const donationInformation = useSelector(
+    state => state.donations.selectedDonationInformation,
   );
   const navigation = useNavigation();
+
+  const categoryInformation = route.params.categoryInformation;
 
   return (
     <SafeAreaView style={[globalStyles.backgroundWhite, globalStyles.flex]}>
       <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
         <BackButton onPress={() => navigation.goBack()} />
+
+        <Image source={{uri: donationInformation.image}} style={style.image} />
+        <View style={style.badge}>
+          <Badge title={categoryInformation.name} />
+        </View>
+
+        <Header type="h1" title={donationInformation.name} />
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
+        <Text style={style.description}>{donationInformation.description}</Text>
       </ScrollView>
+      <View style={style.button}>
+        <Button title="Donate" />
+      </View>
     </SafeAreaView>
   );
 };

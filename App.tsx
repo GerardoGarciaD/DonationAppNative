4aecd7260a37703d4fffa9460e6e8ca66f65f06e
Gerardo <geraz26@hotmail.com>
2023-07-10 22:41:45 -0600

    Wrapping de la app con el store

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import MainNavigation from './Navigation/MainNavigation';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <MainNavigation />
+      </NavigationContainer>
+    </Provider>
   );
 };
 

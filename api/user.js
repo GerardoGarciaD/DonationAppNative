5525345355ec608217c5fbb8ad426ab816004ebd
Gerardo <geraz26@hotmail.com>
2023-07-12 10:55:50 -0600

    Creando funcion para iniciar sesion

--- api/user.js
+++ api/user.js
@@ -15,3 +15,28 @@ export const createUser = async (fullName, email, password) => {
     return {error: 'Something went wrong with your request!'};
   }
 };
+
+export const loginUser = async (email, password) => {
+  try {
+    const response = await auth().signInWithEmailAndPassword(email, password);
+    const token = await response.user.getIdToken();
+    return {
+      status: true,
+      data: {
+        displayName: response.user.displayName,
+        email: response.user.email,
+        token,
+      },
+    };
+  } catch (error) {
+    if (error.code === 'auth/wrong-password') {
+      return {status: false, error: 'Wrong credentials.'};
+    } else if (error.code === 'auth/user-not-found') {
+      return {
+        status: false,
+        error: 'The email you entered does not exists. Please crate an account',
+      };
+    }
+    return {status: false, error: 'Something went wrong with your request!'};
+  }
+};
